b87b0f80bd08ba3fbdefe23cfe74feb63c753cff
Dra-Tammer <2816677369@qq.com>
2026-05-25 10:17:09 +0800

    style: 收紧新建/导入下拉菜单的间距和尺寸

--- awesome_ref_frontend/src/components/ReferenceList.vue
+++ awesome_ref_frontend/src/components/ReferenceList.vue
@@ -197,14 +197,14 @@ function onImportJSON() {
           <Transition name="dropdown">
             <div v-if="showAddMenu" class="add-dropdown">
               <button class="add-dropdown-item" @click="onNewRef">
-                <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
                   <line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/>
                 </svg>
                 <span>新建文献</span>
               </button>
               <div class="add-dropdown-divider"></div>
               <button class="add-dropdown-item" @click="showImportModal = true; showAddMenu = false">
-                <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
                   <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
                   <polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/>
                 </svg>
